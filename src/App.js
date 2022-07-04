db654a7f03b69b70c0c61014be247b8a4333f8ae
usman.brackets <usman.brackets@gmail.com>
2022-07-04 22:42:28 +0500

    changeing text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hgfhgfhdgh</div>;
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
 }
 
 export default App;
